86b06b1855c13d934ec25a47b729c5a102461d54
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-04-08 08:31:40 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,38 @@
+const pack = (date, user, wame) => {
+return `┏━═══ *WZZ BOT* ═══━┓
+║                                                    
+║
+║ _*𝐃𝐚𝐭𝐚 𝐞 𝐡𝐨𝐫𝐚: ${date}*_
+║
+║ _*𝐔𝐬𝐮𝐚́𝐫𝐢𝐨: ${user}*_
+║ 
+║ _*𝐖𝐚𝐦𝐞: ${wame}*_                            
+║                                                    
+║
+┗═══════════════┛
+https://instagram.com/marquinwzz?igshid=ppz93t9ajmgh
+
+━━━━ *ONLYFANS E HENTAIS*  ━━━
+
+❁❧ *FOTOS DE HENTAI 720MB:* 
+_*🌐 https://suaurl.com/55eb2f 🌐*_
+
+❁❧ *PRINCESS STARLING(ONLY FANS)8.36GB:* 
+_*🌐 https://suaurl.com/b995e2 🌐*_
+
+❁❧ *ELLES CLUB 28.68GB:* 
+_*🌐 https://suaurl.com/8c135b 🌐*_
+
+❁❧ *COCONUTKITTY(ONLY FANS)7.69GB:*
+_*🌐 https://suaurl.com/5e91e0 🌐*_
+
+❁❧ *Belle delphine:* 
+_*🌐 https://suaurl.com/5c699d 🌐*_
+
+❁❧ *Love Lilah:* 
+_*🌐 https://suaurl.com/dbaae7 🌐*_
+
+❁❧ *Hidori:* 
+_*🌐 https://suaurl.com/f43f76 🌐*_`
+}
+exports.pack = pack
---
+++ src/wppimune.js
@@ -0,0 +1,64 @@
+const databases = (prefix) => {
+
+return `https://instagram.com/marquinwzz?igshid=ppz93t9ajmgh
+━━━━ ** ━━━━  ━━━━ **  ━━━━ ** ━━ **
+
+┏━━ *DATABASE* ━━┓
+║
+║ ❁❧ ⏤͟͟͞͞𝘿𝘽 𝘿𝙍𝘼𝙂𝙊𝙉 𝙑❷🐉:
+║ https://suaurl.com/9c59ae 
+║
+║ ❁❧ 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 - 𝐊𝐀𝐑𝐋𝐋𝐔𝐒 𝐕𝟔:
+║ https://suaurl.com/f9a181 
+║
+║ ❁❧ DATABASE NESCAU V12:
+║ https://suaurl.com/b677d8 
+║
+║ ❁❧ 𝐃𝐁 𝐌𝐙𝐂 𝐕1:
+║ https://suaurl.com/eb043c 
+║
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+https://instagram.com/marquinwzz?igshid=ppz93t9ajmgh
+━━━━ ** ━━━━  ━━━━ **  ━━━━ ** ━━ **`
+}
+
+exports.databases = databases
+
+const imune = (prefix) => {
+return `
+https://instagram.com/marquinwzz?igshid=ppz93t9ajmgh
+━━━━ ** ━━━━  ━━━━ **  ━━━━ ** ━━ **
+
+┏━━ *WPP IMUNE* ━━┓
+║ ❁❧ ↯𝐈𝐌𝚯𝐑𝐓∆𝐋⛧𝐗𝐗𝐕𝐈ᬏ᭄: 
+║ https://suaurl.com/9332c7 
+║
+║ ❁❧ ♔【𝐃𝐄𝐔𝐒⛧𝐌𝐀𝐃⛧𝐏𝐑𝐈𝐕𝐀𝐓𝐄 𝐕3】♞: 
+║ https://suaurl.com/143052 
+║
+║ ❁❧ DELTA YO EXTREME IMUNE: 
+║ https://suaurl.com/b5b92c 
+║
+║ ❁❧ 𝐏𝐄𝐓𝐀𝐋𝐀𝐙: 
+║ https://suaurl.com/5f75bf 
+║
+║ ❁❧ Sϟ𝚣𝚣𝚎 v5 by BERTH MODER: 
+║ https://suaurl.com/b5c98c 
+║
+║ ❁❧ 𝐓 𝐒 𝐔 𝐊 𝐘: 
+║ https://suaurl.com/e50243 
+║
+║ ❁❧ VΞGΞTΔ ᮃ: 
+║ https://suaurl.com/46d11c 
+║
+║ ❁❧ 𝐗𝐗𝐗 𝀈 𝐂𝐇𝐈𝐊𝐄𝐍: 
+║ https://suaurl.com/2d3fd0 
+║
+║ ❁❧ ᬊ͜͡✪𝑺𝒉𝒂𝒅𝒐𝒘 𝐖𝚨⁹⁹⁹⁺.𝑴𝒂𝒕𝒉𝒆𝒖𝒔 𝑹𝒆𝒎𝒐𝒅𝒔: 
+║ https://suaurl.com/265415 
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+https://instagram.com/marquinwzz?igshid=ppz93t9ajmgh
+━━━━ ** ━━━━  ━━━━ **  ━━━━ ** ━━ **`
+}
+
+exports.imune = imune
